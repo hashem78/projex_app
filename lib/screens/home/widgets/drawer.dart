4ebd3a22e36cb9aa2597ec9c501572da470578f9
hashem78 <hashem.olayano@gmail.com>
2022-05-05 03:38:43 +0300

    SImplify obtaining puser instances in the widget tree

--- lib/screens/home/widgets/drawer.dart
+++ lib/screens/home/widgets/drawer.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:projex_app/screens/profile/widgets/profile_screen_image.dart';
-import 'package:projex_app/state/auth.dart';
+import 'package:projex_app/screens/profile/widgets/puser_builder.dart';
 import 'package:projex_app/state/locale.dart';
 
 class PDrawer extends ConsumerWidget {
@@ -12,54 +12,38 @@ class PDrawer extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final userFuture = ref.watch(pCurrentUserProvider);
     final translations = ref.watch(translationProvider).translations;
 
     return Drawer(
       child: ListView(
         children: [
-          userFuture.when(
-            data: (user) {
-              if (user != null) {
-                return GestureDetector(
-                  onTap: () {
-                    context.go('/profile/${user.id}');
-                  },
-                  child: UserAccountsDrawerHeader(
-                    currentAccountPicture: ProfileScreenImage(
-                      user: user,
-                      borderWidth: 2,
+          PUserBuilder.fromCurrent(
+            builder: (context, user) => GestureDetector(
+              onTap: () {
+                context.go('/profile/${user.id}');
+              },
+              child: UserAccountsDrawerHeader(
+                currentAccountPicture: ProfileScreenImage(
+                  user: user,
+                  borderWidth: 2,
+                ),
+                accountName: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      user.map(
+                        student: (std) => translations.profile.studentType,
+                        instructor: (inst) =>
+                            translations.profile.instructorType,
+                      ),
                     ),
-                    accountName: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          user.map(
-                            student: (std) => translations.profile.studentType,
-                            instructor: (inst) =>
-                                translations.profile.instructorType,
-                          ),
-                        ),
-                        Text(user.name),
-                      ],
-                    ),
-                    accountEmail: Text(user.email),
-                  ),
-                );
-              } else {
-                ref.refresh(pCurrentUserProvider);
-                return Container();
-              }
-            },
-            error: (err, st) {
-              debugPrint(err.toString());
-              debugPrint(st.toString());
-              return Container();
-            },
-            loading: () {
-              return Container();
-            },
+                    Text(user.name),
+                  ],
+                ),
+                accountEmail: Text(user.email),
+              ),
+            ),
           ),
           ListTile(
             leading: const Icon(Icons.settings),
--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:projex_app/screens/profile/widgets/profile_card.dart';
 import 'package:projex_app/screens/profile/widgets/profile_screen_appbar.dart';
+import 'package:projex_app/screens/profile/widgets/puser_builder.dart';
 
-import 'package:projex_app/state/auth.dart';
-
-class ProfileScreen extends ConsumerWidget {
+class ProfileScreen extends StatelessWidget {
   const ProfileScreen({
     Key? key,
     required this.uid,
@@ -14,27 +12,21 @@ class ProfileScreen extends ConsumerWidget {
   final String uid;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final userFuture = ref.watch(pUserProvider(uid));
-
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: userFuture.when(
-          data: (user) {
-            if (user != null) {
-              return [
-                ProfileScreenAppBar(user: user),
-                SliverPadding(
-                  padding: EdgeInsets.only(top: 0.065.sh, left: 12, right: 12),
-                  sliver: ProfileCard(user: user),
-                ),
-              ];
-            }
-            return const [];
-          },
-          error: (_, __) => const [],
-          loading: () => const [],
-        ),
+      body: PUserBuilder.fromUid(
+        uid: uid,
+        builder: (context, user) {
+          return CustomScrollView(
+            slivers: [
+              ProfileScreenAppBar(user: user),
+              SliverPadding(
+                padding: EdgeInsets.only(top: 0.065.sh, left: 12, right: 12),
+                sliver: ProfileCard(user: user),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
