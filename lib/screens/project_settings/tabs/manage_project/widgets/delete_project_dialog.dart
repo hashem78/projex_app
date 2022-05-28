db276d4873dac7d1658d21d2a0228df3d437c93a
hashem78 <hashem.olayano@gmail.com>
2022-05-28 03:06:36 +0300

    Fix crash when deleting projects

--- lib/screens/project_settings/tabs/manage_project/widgets/delete_project_dialog.dart
+++ lib/screens/project_settings/tabs/manage_project/widgets/delete_project_dialog.dart
@@ -1,12 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:projex_app/state/project_provider.dart';
 
-class DeleteProjectDialog extends StatelessWidget {
+class DeleteProjectDialog extends ConsumerWidget {
   const DeleteProjectDialog({
     Key? key,
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return AlertDialog(
       title: const Text('Delete this project?'),
       content: const Text(
@@ -18,7 +22,11 @@ class DeleteProjectDialog extends StatelessWidget {
           child: const Text('No'),
         ),
         TextButton(
-          onPressed: () => Navigator.of(context).pop(true),
+          onPressed: () {
+            final pid = ref.read(selectedProjectProvider);
+            context.go('/');
+            FirebaseFirestore.instance.doc('/projects/$pid').delete();
+          },
           child: const Text('Yes'),
         ),
       ],
--- lib/screens/project_settings/tabs/manage_project/widgets/delete_project_tile.dart
+++ lib/screens/project_settings/tabs/manage_project/widgets/delete_project_tile.dart
@@ -1,37 +1,30 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
+
 import 'package:projex_app/screens/project_settings/tabs/manage_project/widgets/delete_project_dialog.dart';
 import 'package:projex_app/state/project_provider.dart';
 
-class DeleteProjectTile extends ConsumerStatefulWidget {
+class DeleteProjectTile extends ConsumerWidget {
   const DeleteProjectTile({
     Key? key,
   }) : super(key: key);
 
   @override
-  ConsumerState<DeleteProjectTile> createState() => _DeleteProjectTileState();
-}
-
-class _DeleteProjectTileState extends ConsumerState<DeleteProjectTile> {
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Align(
       alignment: Alignment.bottomLeft,
       child: TextButton.icon(
-        onPressed: () async {
-          final delete = await showDialog<bool>(
-                context: context,
-                builder: (context) => const DeleteProjectDialog(),
-              ) ??
-              false;
-          if (delete) {
-            final pid = ref.read(selectedProjectProvider);
-            await FirebaseFirestore.instance.doc('/projects/$pid').delete();
-            if (!mounted) return;
-            context.push('/');
-          }
+        onPressed: () {
+          final id = ref.read(selectedProjectProvider);
+          showDialog(
+            context: context,
+            builder: (context) => ProviderScope(
+              overrides: [
+                selectedProjectProvider.overrideWithValue(id),
+              ],
+              child: const DeleteProjectDialog(),
+            ),
+          );
         },
         icon: const Icon(
           Icons.delete,
